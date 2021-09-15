ad7e2a8a9eb63a8826eef5ebd267523a269e66bf
Luigy Leon <luigy@outlook.com>
2021-09-15 17:23:11 -0400

    ghc810: ob run works

--- default.nix
+++ default.nix
@@ -18,10 +18,15 @@ with obelisk;
 let
 deps = obelisk.nixpkgs.thunkSet ./dep;
 rhyolite = (import deps.rhyolite { inherit obelisk; });
-p = project ./. ({ pkgs, ... }: {
+p = project ./. ({ pkgs, ... }: let haskellLib = pkgs.haskell.lib; in {
   overrides = pkgs.lib.composeExtensions
     rhyolite.haskellOverrides
-    (_: _: {});
+    (self: super: {
+      beam-core = haskellLib.doJailbreak super.beam-core; # upstream to reflex-platform; also update aeson to track >=1.5
+      beam-migrate = haskellLib.doJailbreak super.beam-migrate; # upstream to reflex-platform; also update aeson to track >=1.5
+      beam-automigrate = haskellLib.doJailbreak (self.callHackage "beam-automigrate" "0.1.2.0" {}); # upstream to reflex-platform; also update aeson to track >=1.5
+      beam-postgres = haskellLib.doJailbreak super.beam-postgres; # upstream to reflex-platform; also update aeson to track >=1.5
+    });
   android.applicationId = "systems.obsidian.obelisk.examples.minimal";
   android.displayName = "Obelisk Minimal Example";
   ios.bundleIdentifier = "systems.obsidian.obelisk.examples.minimal";
