7a6168c7d64ea70a3be1ec6ebbf5c5b1a302dd2e
Luigy Leon <luigy@outlook.com>
2021-09-16 07:56:19 -0400

    nix-build -A ghc.cardano-ledger-core

--- default.nix
+++ default.nix
@@ -35,8 +35,31 @@ p = project ./. ({ pkgs, ... }: let haskellLib = pkgs.haskell.lib; in {
       cardano-prelude = self.callCabal2nix "cardano-prelude" (deps.cardano-prelude + "/cardano-prelude") {};
       cardano-prelude-test = self.callCabal2nix "cardano-prelude-test" (deps.cardano-prelude + "/cardano-prelude-test") {};
       cardano-binary = haskellLib.enableCabalFlag (self.callCabal2nix "cardano-binary" (deps.cardano-base + "/binary") {}) "development";
+      cardano-binary-test = haskellLib.enableCabalFlag (self.callCabal2nix "cardano-binary-test" (deps.cardano-base + "/binary/test") {}) "development";
       cardano-slotting = self.callCabal2nix "cardano-slotting" (deps.cardano-base + "/slotting") {};
+      cardano-crypto-praos = haskellLib.addPkgconfigDepend (self.callCabal2nix "cardano-crypto-praos" (deps.cardano-base + "/cardano-crypto-praos") {}) pkgs.libsodium; # TODO use libsodium-vrf fork
+      cardano-crypto-class = haskellLib.addPkgconfigDepend (self.callCabal2nix "cardano-crypto-class" (deps.cardano-base + "/cardano-crypto-class") {}) pkgs.libsodium; # TODO use libsodium-vrf fork
+      strict-containers = self.callCabal2nix "strict-containers" (deps.cardano-base + "/strict-containers") {};
+      cardano-crypto-wrapper = haskellLib.dontCheck (self.callCabal2nix "cardano-crypto-wrapper" (deps.cardano-ledger-specs + "/byron/crypto") {});
+      # tests fail on some env var not being set
+      cardano-ledger-byron = haskellLib.dontCheck (haskellLib.enableCabalFlag (haskellLib.doJailbreak (self.callCabal2nix "cardano-ledger-byron" (deps.cardano-ledger-specs + "/byron/ledger/impl") {})) "development");
+      cardano-ledger-core = haskellLib.dontCheck (self.callCabal2nix "cardano-ledger-core" (deps.cardano-ledger-specs + "/cardano-ledger-core") {});
+      shelley-spec-non-integral = haskellLib.dontCheck (self.callCabal2nix "shelley-spec-non-integral" (deps.cardano-ledger-specs + "/shelley/chain-and-ledger/dependencies/non-integer") {});
+      small-steps = haskellLib.dontCheck (self.callCabal2nix "small-steps" (deps.cardano-ledger-specs + "/semantics/executable-spec") {});
+      small-steps-test = haskellLib.dontCheck (haskellLib.doJailbreak (self.callCabal2nix "small-steps-test" (deps.cardano-ledger-specs + "/semantics/small-steps-test") {}));
+      byron-spec-chain = haskellLib.dontCheck (haskellLib.doJailbreak (self.callCabal2nix "byron-spec-chain" (deps.cardano-ledger-specs + "/byron/chain/executable-spec") {}));
+      byron-spec-ledger = haskellLib.dontCheck (haskellLib.doJailbreak (self.callCabal2nix "byron-spec-ledger" (deps.cardano-ledger-specs + "/byron/ledger/executable-spec") {}));
+      cardano-crypto-test = haskellLib.doJailbreak (haskellLib.dontCheck (self.callCabal2nix "cardano-crypto-test" (deps.cardano-ledger-specs + "/byron/crypto/test") {}));
+      contra-tracer = haskellLib.dontCheck (self.callCabal2nix "contra-tracer" (deps.iohk-monitoring-framework + "/contra-tracer") {});
       nothunks = self.callHackage "nothunks" "0.1.0.0" {};
+      goblins = haskellLib.dontCheck (self.callCabal2nix "goblins" deps.goblins {});
+      moo = haskellLib.markUnbroken super.moo;
+      gray-code = haskellLib.overrideCabal (haskellLib.markUnbroken super.gray-code) {
+        preCompileBuildDriver = "rm Setup.hs";
+      };
+      typerep-map = haskellLib.markUnbroken super.typerep-map;
+      primitive = haskellLib.dontCheck (self.callHackage "primitive" "0.7.1.0" {}); # cardano-crypto-class min bound
+      streaming-bytestring = self.callHackage "streaming-bytestring" "0.2.1" {}; # cardano-crypto-class min bound
       # prop_aeson_canonical:     FAIL                            │
       # *** Failed! Falsified (after 12 tests and 4 shrinks):   │
       # JSObject [("\CAN$%OW",JSObject [])]                     │
